06f0c0ec553bfe91c81e3358d1e19fccea261105
Bryan Oliande <boliande@uci.edu>
2017-08-18 21:01:39 -0700

    Texture Mapping in OpenGL
    Texture Mapping in OpenGL

---
+++ inputModule.h
@@ -0,0 +1,24 @@
+// Oliande, Bryan: 13179240
+// Tryon, Daniel: 20621204
+
+#ifndef INPUTMODULE_H
+#define INPUTMODULE_H
+
+#include <GLUT/glut.h>
+
+/*---------------external entities from sceneModule-----------------*/
+extern GLfloat angley;   
+extern GLfloat anglex;  
+extern GLfloat distanceX;
+extern GLfloat distanceY;
+extern GLfloat distanceZ;
+
+/*---------------external entities from main-------------------------*/
+extern void cleanup();
+
+void readKeyboard( unsigned char key, int x, int y );
+void readSpecialKeys( int key, int x, int y );
+void mouseButtHandler( int button, int state, int x, int y );
+void mouseMoveHandler( int x, int y );
+
+#endif // INPUTMODULE_H
---
+++ sceneModule.h
@@ -0,0 +1,14 @@
+// Oliande, Bryan: 13179240
+// Tryon, Daniel: 20621204
+
+
+#ifndef SCENEMODULE_H
+#define SCENEMODULE_H
+
+#include <GLUT/glut.h>
+
+
+void drawScene();
+void display();
+
+#endif // SCENEMODULE_H
